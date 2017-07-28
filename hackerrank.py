279fcb1d99b84ccd22f824ff1af4515c20a2dcba
Michelle <jhmichellekim@gmail.com>
2017-07-27 17:43:29 -0700

    added birthdaycakecandles, timeconversion

--- hackerrank.py
+++ hackerrank.py
@@ -11,4 +11,34 @@ def compete_scores(a0, a1, a2, b0, b1, b2):
 
 
 def birthdayCakeCandles(n, ar):
+    # only the tallest candles are 
+    # ar = array of heights of each candle
     return ar.count(max(ar))
+
+
+def timeConversion(s):
+    # 07:05:45PM -> 19:05:45
+    if s[-2] == "A":
+        if s[:2] == "12":
+            return "00" + s[2:-2]
+        else:
+            return s[:-2]
+    else:
+        if s[:2] == "12":
+            return s[:-2]
+        return str(int(s[:2]) + 12) + s[2:-2]
+
+
+def time_conversion(s):
+    # 07:05:45PM -> 19:05:45
+    if s[-2] == "A":
+        if s[:2] == "12":
+            return "00" + s[2:-2]
+        else:
+            pass
+    else:
+        if s[:2] == "12":
+            pass
+        else:
+            return str(int(s[:2]) + 12) + s[2:-2]
+    return s[:-2]
