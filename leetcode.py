7149a4503a31b9d8139c517251b80dc9a8dedf17
Michelle <jhmichellekim@gmail.com>
2017-08-08 13:02:08 -0700

    created making calendar file

--- leetcode.py
+++
@@ -1,4 +0,0 @@
-# logistics system where a client places an order to get items delivered to a given destination
-# keep track of statuses of all the orders
-# admin of logistics charges some amount for delivering the orders
-
